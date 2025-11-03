37b9ed774243632d0d0bde8c55f7c6c246c6cfaa
PriyanshuPathak09 <priyanshupathak708@gmail.com>
2025-11-03 20:47:54 +0530

    100 days of code

--- Q9.c
+++ Q9.c
@@ -3,20 +3,29 @@
 
 int main() 
 {
-    float principal, rate, time, simpleInterest, compoundInterest;
+    double principal, rate, time, simpleInterest, compoundInterest;
 
     printf("Enter Principal amount: ");
-    scanf("%f", &principal);
+    if (scanf("%lf", &principal) != 1 || principal < 0) {
+        printf("Error: Please enter a non-negative principal amount\n");
+        return 1;
+    }
 
     printf("Enter Rate of interest (in %%): ");
-    scanf("%f", &rate);
+    if (scanf("%lf", &rate) != 1 || rate < 0) {
+        printf("Error: Please enter a non-negative interest rate\n");
+        return 1;
+    }
 
     printf("Enter Time (in years): ");
-    scanf("%f", &time);
+    if (scanf("%lf", &time) != 1 || time < 0) {
+        printf("Error: Please enter a non-negative time period\n");
+        return 1;
+    }
 
-    simpleInterest = (principal * rate * time) / 100;
+    simpleInterest = (principal * rate * time) / 100.0;
 
-    compoundInterest = principal * pow((1 + rate / 100), time) - principal;
+    compoundInterest = principal * pow((1.0 + rate / 100.0), time) - principal;
 
     printf("Simple Interest = %.2f\n", simpleInterest);
     printf("Compound Interest = %.2f\n", compoundInterest);
